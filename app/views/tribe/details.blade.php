4d07851b37fb85a8216adeaa9839537fad19026d
Femi Veys <femi@tribeforce.com>
2013-07-03 22:12:40 +0200

    Make buttons of person links

--- app/views/tribe/details.blade.php
+++ app/views/tribe/details.blade.php
@@ -6,8 +6,8 @@
 @section('main')
 <div id="tribe-details">
   <h2>@lang("ui.about")</h2>
-  {{ link_to_action('FriendsController@getGoals', trans('ui.goals.title_index'), $d->id) }}
-  {{ link_to_action('FriendsController@getEndorsements', trans('ui.endorsements.title_index'), $d->id) }}
+  {{ link_to_action('FriendsController@getGoals', trans('ui.goals.title_index'), $d->id, array('class' => 'button small')) }}
+  {{ link_to_action('FriendsController@getEndorsements', trans('ui.endorsements.title_index'), $d->id, array('class' => 'button small')) }}
   <div data-section-content class="row">
     <div class="columns small-12 large-6">
     @include('field', array('name' => 'avatar'))
